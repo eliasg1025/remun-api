1e5c8ef90ef622b3b2dc0c2534cbc4a28dbb065d
Elias Guere <eliasguere1025@gmail.com>
2020-11-01 18:13:34 -0500

    Corrigiendo tomar el valor de paymentId

--- app/Services/LecturasSueldoService.php
+++ app/Services/LecturasSueldoService.php
@@ -8,19 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class LecturasSueldoService
 {
-    public function store(Carbon $periodo, $tipoPagoId, $usuarioId)
+    public function store(Carbon $periodo, $tipoPagoId, $usuarioId, $pagoId)
     {
-        $pago = DB::table('pagos')->where([
-            'mes' => $periodo->month,
-            'anio' => $periodo->year,
-            'tipo_pago_id' => $tipoPagoId
-        ])->first();
-
         DB::table('lecturas_sueldo')->insert([
             'fecha_hora'    => now()->toDateTimeString(),
             'usuario_id'    => $usuarioId,
             'tipo_pago_id'  => $tipoPagoId,
-            'pago_id'       => $pago->id
+            'pago_id'       => $pagoId,
         ]);
     }
 }
