8631fa92a2f48697a2659f9ffcc4e837ac0128ea
Elias Guere <eliasguere1025@gmail.com>
2020-11-02 11:37:33 -0500

    #

--- app/Http/Controllers/AsistenciasController.php
+++ app/Http/Controllers/AsistenciasController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\ProcessStoreManyAsistencias;
+use App\Services\AsistenciasService;
 use Illuminate\Http\Request;
 
 class AsistenciasController extends Controller
 {
+    private $service;
+
     public function __construct()
     {
-
+        $this->service = new AsistenciasService();
     }
 
     public function storeMany(Request $request)
@@ -19,7 +22,9 @@ class AsistenciasController extends Controller
             'mes'  => $request->get('mes'),
             'anio' => $request->get('anio')
         ];
-        $result = ProcessStoreManyAsistencias::dispatch($data['data']);
+        //$result = ProcessStoreManyAsistencias::dispatch($data['data']);
+
+        $this->service->storeMany($data['data']);
         return response()->json([
             'message' => 'Proceso en cola'
         ]);
