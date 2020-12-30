8353c51876424223f4103089c0e5c8c813a7f137
Elias Guere <eliasguere1025@gmail.com>
2020-12-30 10:44:04 -0500

    Agregando endpoint para obtener roles/modulos de un usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -33,5 +34,29 @@ class AuthController extends Controller
         $usuario = $request->get('user');
 
         return response()->json($usuario);
+    }
+
+    public function roles(Request $request)
+    {
+        $usuario = $request->get('user');
+
+        $admUsuarioRoles = DB::table('adm_usuario_roles')
+            ->where('usuario_id', $usuario->sub)
+            ->get();
+
+        $admUsuarioRoles->transform(function ($item) {
+            $item->modulo = DB::table('adm_modulos')->where('id', $item->modulo_id)->first();
+
+            $item->rol = DB::table('adm_roles')->where([
+                'id' => $item->rol_id,
+                'modulo_id' => $item->modulo_id
+            ])->first();
+
+            return $item;
+        });
+
+        return response()->json([
+            'data' => $admUsuarioRoles
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group(['prefix' => 'auth'], function() {
     Route::post('login', [AuthController::class,'login']);
     Route::post('logout', [AuthController::class,'logout']);
     Route::get('me', [AuthController::class, 'me'])->middleware('api.auth');
+    Route::get('roles', [AuthController::class, 'roles'])->middleware('api.auth');
 });
 
 Route::group(['prefix' => 'roles'], function() {
